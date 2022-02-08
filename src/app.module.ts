e7963447e4477912b5d7ef2a8179d5f9e8c7ddb9
nursSSS <nurik1kun@gmail.com>
2022-02-08 21:02:46 +0600

    created Controller, Service, Module and inteface of links

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LinkController } from './link/link.controller';
+import { LinkModule } from './link/link.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://nurs:123321@cluster0.nfrrm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
-  controllers: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://nurs:123321@cluster0.nfrrm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'), LinkModule],
+  controllers: [LinkController],
   providers: [],
 })
 export class AppModule {}

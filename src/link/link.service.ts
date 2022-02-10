aa74eff10d533e8c2b56aa7fe18ee03561bf602b
NursSSS <nurik1kun@gmail.com>
2022-02-10 18:10:37 +0600

    created findAll links by email and fixed errors with modules

--- src/link/link.service.ts
+++ src/link/link.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { LinkEntity } from './entity/link.entity';
+import { ILink } from './interface/link.interface';
 
 @Injectable()
 export class LinkService {
@@ -10,5 +11,7 @@ export class LinkService {
         private readonly entity: Model<LinkEntity>
       ) {}
     
-    
+    async findAll(email: string){
+        return await this.entity.find((item: ILink) => item.email == email)
+    }
 }

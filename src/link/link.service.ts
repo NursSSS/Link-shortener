fd72f178d3d00c3769c66126db796d0128ba740e
NursSSS <nurik1kun@gmail.com>
2022-02-10 23:14:51 +0600

    created delete api, fixed some bags and added cheks for methods

--- src/link/link.service.ts
+++ src/link/link.service.ts
@@ -1,8 +1,9 @@
-import { ConflictException, HttpStatus, Injectable, Redirect } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateLinkDto } from './dto';
 import { LinkEntity } from './entity/link.entity';
+import { ILink } from './interface/link.interface';
 
 @Injectable()
 export class LinkService {
@@ -18,17 +19,34 @@ export class LinkService {
     async createShortLink(dto: CreateLinkDto){
         let randomstring = require("randomstring")
         dto._key = randomstring.generate(7)
-        dto.short_link = 'http://localhost:3000/link/re' + dto._key
+        dto.short_link = 'http://localhost:3000/link/' + dto._key
         return await this.entity.create(dto)
     }
 
     async filter(mail: string){
-        return await this.entity.find( {email: mail} )
+        const link = this.entity.find( {email: mail} )
+        if(!link){
+            throw new NotFoundException()
+        }
+
+        return await link
     }
 
-    
     async redirect(key: string){
         const link = await this.entity.findOne( {_key: key} )
+        if(!link){
+            throw new NotFoundException()
+        }
+
         return { statusCode: 302, url: link.original_link}
+    }
+
+    async deleteOne(id: string){
+        const link = await this.entity.findById(id)
+        if(!link){
+            throw new NotFoundException()
+        }
+
+        return await this.entity.deleteOne( {link} )
     }
 }
